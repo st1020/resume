643fb1f504cd5500aad2dd8276a1b8fc85bf4669
st1020 <me@st1020.com>
2026-01-08 19:27:32 +0800

    chore: update unocss version and use bun as package manager

--- uno.config.ts
+++ uno.config.ts
@@ -1,15 +1,65 @@
-import fs from "fs/promises";
-import { defineConfig, presetIcons, presetUno, transformerDirectives } from "unocss";
+import { defineConfig, presetIcons, presetWind4 } from "unocss";
+
+const progressPreflightCss = `
+progress {
+  border-radius:9999px;
+  background-color:rgb(207 250 254);
+}
+progress::-webkit-progress-value {
+  border-radius:9999px;
+  background-color:rgb(14 116 144);
+}
+progress::-webkit-progress-bar {
+  border-radius:9999px;
+  background-color:rgb(207 250 254);
+}
+progress::-moz-progress-bar {
+  border-radius:9999px;
+  background-color:rgb(14 116 144);
+}`;
 
 export default defineConfig({
+  cli: {
+    entry: {
+      patterns: ["templates/**/*.html", "content/**/*.toml"],
+      outFile: "static/main.css",
+      // @ts-ignore
+      rewrite: true,
+    },
+  },
   preflights: [
     {
-      getCSS: () => fs.readFile("node_modules/@unocss/reset/tailwind.css", "utf-8"),
+      getCSS: ({ theme }) => {
+        // @ts-ignore
+        const cyan100 = theme.colors.cyan[100] as string;
+        // @ts-ignore
+        const cyan700 = theme.colors.cyan[700] as string;
+        return `
+progress {
+  border-radius: 9999px;
+  background-color: ${cyan100};
+}
+progress::-webkit-progress-value {
+  border-radius: 9999px;
+  background-color: ${cyan700};
+}
+progress::-webkit-progress-bar {
+  border-radius: 9999px;
+  background-color: ${cyan100};
+}
+progress::-moz-progress-bar {
+  border-radius: 9999px;
+  background-color: ${cyan700};
+}`;
+      },
     },
   ],
-  content: {
-    filesystem: ["templates/**/*.html", "content/**/*.toml"],
-  },
-  presets: [presetUno(), presetIcons()],
-  transformers: [transformerDirectives()],
+  presets: [
+    presetIcons(),
+    presetWind4({
+      preflights: {
+        reset: true,
+      },
+    }),
+  ],
 });
